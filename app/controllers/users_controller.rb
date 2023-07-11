d43a85a85062091fb1e11b0e2b6817b82c03a600
Lucas Simpson <lucassimpson0213@gmail.com>
2023-07-11 17:19:22 -0500

    added changes to the users model class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
-class UsersController
-  def index ()
+class UsersController < ApplicationController
+
+  validate
+
+
+  def index
     @users = Users.all
   end
+
+
+  def create
+    @users = Users.new(users_params)
+  end
 end
+
+private
+
+def users_params
+  params.require(:users).permit(:name,:password)
+end
+
+
+
+
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users < ApplicationRecord
-  
+  attribute :name, :string
+  attribute :email, :string
 
 end
