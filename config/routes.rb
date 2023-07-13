2e8272eddad947c309c2d6ddd2a85cc0fc18e9c7
Lucas Simpson <lucassimpson0213@gmail.com>
2023-07-13 13:02:56 -0500

    rolled backed to previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
-  resources :users
-
-
-
-
-end
-=======
 
   Rails.application.routes.draw do
     root 'posts#index'
     resources :posts
+    
 
 
   end
 
->>>>>>> 40bcedbd67370f9c55e2630e070a1c547da4711a
